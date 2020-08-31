776b0cc86e2655c31bff86bdcd15f4aaffc6f463
Esha-Sherring <esherring2016@gmail.com>
2020-08-31 21:43:05 +0530

    Simplified version of to do list

---
+++ forRepo/app.js
@@ -0,0 +1,53 @@
+const express =require("express");
+const bodyParser=require("body-parser");
+const app=express();
+var items=["groceries","bills","pet food"];
+var workItems=[];
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+
+    var today=new Date();
+    var options={
+    	weekday: "long",
+    	day: "numeric",
+    	month: "long"
+    	    };
+    	    var day=today.toLocaleDateString("en-US",options);
+    res.render("list",{listTitle: day, newListItems:items
+ });
+        
+});
+app.get("/work",function(req,res){
+
+   
+    res.render("list",{listTitle: "Work List", newListItems:workItems
+ });
+        
+});
+app.post("/",function(req,res){
+	var item=req.body.newItem;
+	if(req.body.list=="Work")
+	{
+		workItems.push(item);
+		res.redirect("/work");
+
+	}
+	else
+	{
+		items.push(item);
+		res.redirect("/");
+	}
+
+});
+app.post("/work",function(req,res){
+	var item=req.body.newItem;
+	items.push(item);
+	res.redirect("/");
+
+});
+app.listen(3000,function(){
+  console.log("server running on port 3000");
+});
+
